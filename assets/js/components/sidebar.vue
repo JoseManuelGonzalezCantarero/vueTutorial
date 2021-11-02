be6468bc91f812517846e311f5963301bc5067b1
manuel.gonzalez <manuel.gonzalez@check24.es>
2021-11-02 15:26:14 +0100

    Using Shared CSS

--- assets/js/components/sidebar.vue
+++ assets/js/components/sidebar.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        :class="componentClass"
+        :class="[$style.component, 'p-3', 'mb-5']"
     >
         <div v-if="!collapsed">
             <h5 class="text-center">
@@ -54,19 +54,6 @@ export default {
             ],
         };
     },
-    computed: {
-        /**
-         * Computes the component classes depending on collapsed state
-         * @returns string[]
-         */
-        componentClass() {
-            const classes = [this.$style.component, 'p-3', 'mb-5'];
-            if (this.collapsed) {
-                classes.push(this.$style.collapsed);
-            }
-            return classes;
-        },
-    }
 };
 </script>
 
@@ -74,9 +61,6 @@ export default {
 @import '~styles/components/light-component';
 .component {
   @include light-component;
-  &.collapsed {
-    width: 70px;
-  }
   ul {
     li a:hover {
       background: $blue-component-link-hover;
